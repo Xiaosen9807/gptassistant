bca766560ae595e2dd21e63f9d18c6ebc1bb040b
Curti Mitrofan <curti.mitrofan@gmail.com>
2023-05-11 20:23:02 +0200

    pdf scanning enabled

--- GPTlib.py
+++ GPTlib.py
@@ -1,3 +1,5 @@
+import fitz, openai, os
+
 
 def extract_last_prompt_and_answer(filename):
     """Function to extract the content of the last prompt and answer from a text file"""
@@ -27,4 +29,73 @@ def extract_last_prompt_and_answer(filename):
 def add_response_to_file(filename, response):
     with open(filename, 'a', encoding='utf-8') as file_obj:
         file_obj.write(f"\nanswer:\n{response.choices[0].text.strip()}\ntokens used: {response.usage.total_tokens}\n")
-                
+
+def completion(prompt):
+
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    
+
+    response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt=prompt,
+      temperature=0.8,
+      max_tokens=3000,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
+    )
+    
+    return response
+
+def get_words(text, start, end):
+    start_idx = text.find(start)
+    end_idx = text.find(end)
+    
+    if start_idx == -1 or end_idx == -1:
+        return False
+    else:
+        return text[start_idx+len(start):end_idx]
+    
+
+def pdf_highlight(filename, section_list, checks):
+    section_list = section_list.split(', ')
+    print(section_list)
+    doc = fitz.open(filename)
+    text = ''
+    for page in doc:
+        text += page.get_text().replace('\n', ' ')
+        
+    for start_word, end_word in zip(section_list[0:-1], section_list[1:]):
+        section = get_words(text, start_word, end_word)
+        
+        if section == False:
+            print("No sections found")
+        else:
+            print(f"Section {start_word} \n\n" + section)
+            response = completion("\n" + checks + "\n" + section)
+            print(f"\ncorrections: \n" + response.choices[0].text.strip() + '\n')
+            
+            with open(filename[0:-3]+'txt', 'a', encoding='utf-8') as file_obj:
+                file_obj.write(f"Section {start_word} \n\n")
+                file_obj.write(response.choices[0].text.strip() + '\n')
+
+
+def extract_instructions(filename):
+  with open(filename, 'r') as f:
+    data = f.readlines()
+    filename = ''
+    section_list = ''
+    instructions = ''
+    for line in data:
+      if line.startswith('filename'):
+        filename = line.replace('filename:','').strip()
+      elif line.startswith('section_list'):
+        section_list = line.split(':')[1].strip()
+      elif line.startswith('instructions'):
+        instructions = line.split(':')[1].strip()
+      else:
+        instructions += "\n" + line.strip()
+    print(filename)
+    print(section_list)
+    print(instructions)
+    return filename, section_list, instructions
--- main.py
+++ main.py
@@ -1,28 +1,44 @@
-import os
-import openai
+#!/usr/bin/env python
+
+import argparse
 import GPTlib as f
 
+# Define command-line arguments 
+parser = argparse.ArgumentParser(description="This is a python script to run specific functions from it in the CLI")
+parser.add_argument("--function",
+                    help = "Name of the function to be executed")
 
-def completion():
+parser.add_argument("-fn", "--filename",
+                    default = "../GPT/document_scan.txt",
+                    help = "Filename used to scan the documents")
 
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+parser.add_argument("-fc", "--filename_completion",
+                    default = '../GPT/messages.txt',
+                    help = "Filename used to scan the documents")
 
-    filename = '../GPT/messages.txt'
+# Parse the command-line arguments
+args = parser.parse_args()
+
+
+
+def completion(filename = args.filename_completion):
 
     prompt = f.extract_last_prompt_and_answer(filename)
     
-
-    response = openai.Completion.create(
-      model="text-davinci-003",
-      prompt=prompt,
-      temperature=1,
-      max_tokens=4000,
-      top_p=1,
-      frequency_penalty=0,
-      presence_penalty=0
-    )
-
+    response = f.completion(prompt)
+    
     f.add_response_to_file(filename, response)
 
-if __name__ == '__main__':
-    completion()
+def highlight(filename = args.filename):
+    
+    filename, section_list, instructions = f.extract_instructions(filename)
+    f.pdf_highlight(filename, section_list, instructions)
+
+# Map the command-line argument with the functions
+function_dict = {
+    'completion' : completion,
+    'highlight' : highlight
+    }
+
+# Execute the function
+function_dict.get(args.function)()
